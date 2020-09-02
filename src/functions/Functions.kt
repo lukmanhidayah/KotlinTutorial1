dc6e4b852629b2aa6d52ab267047207c725bc61b
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-09-03 03:54:34 +0700

    Adding function to my journey of kotlin

--- src/functions/Functions.kt
+++ src/functions/Functions.kt
@@ -1,14 +1,35 @@
 package functions
 
+import java.util.*
+
+// function with no parameter and no return
 fun helloWorld() {
     println("Hello world")
 }
 
-//function with parameter
-fun printWithSpaces(text: String){
+//function with parameter, no return
+fun printWithSpaces(text: String) {
+    for (char in text) {
+        print("$char ")
+    }
+}
 
+//specify return type, must have return but can't have parameter
+fun getCurrentDate(): Date {
+    return Date()
+}
+
+//specify return type, must have return and have parameter
+fun max(a: Int, b: Int): Int{
+    return if(a >= b)
+        a
+    else
+        b
 }
 
 fun main(args: Array<String>) {
     helloWorld()
+    printWithSpaces("Kotlin is easy to use")
+    println(getCurrentDate())
+    println(max(5,2))
 }
